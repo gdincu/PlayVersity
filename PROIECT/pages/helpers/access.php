fa5c5c6c825c4784acd67a89cad5a88c617814d5
gdincu <dici0137@scs.ubbcluj.ro>
2020-05-21 09:38:46 +0300

    Added a user login/ logout form (Session Management)

--- PROIECT/pages/helpers/access.php
+++ PROIECT/pages/helpers/access.php
@@ -2,13 +2,11 @@
 require_once (__DIR__ . '/../../db_setup/db_connect.php');
 require_once "session.php";
 
-$ascheck = $descheck = true;
-
-if (isset($_POST["access"]) ) {	
+if (isset($_POST["access"])) {	
+	
 	if(isLoggedIn()) {
 		session_destroy();
-		header("Location: index.php");
-		// echo "Logout reusit!";
+		header("Location: index.php?page=home&error=LoggedOut");
 		exit;
 	}
 
@@ -17,13 +15,12 @@ if (isset($_POST["access"]) ) {
 	$userFinal = "'".htmlentities($_POST["username"],ENT_HTML5,'UTF-8',TRUE)."'";
 	$passwordFinal =  "'".hash("sha256", htmlentities($_POST["password"],ENT_HTML5,'UTF-8',TRUE))."'";
 	
-	$sql = "SELECT username password FROM user WHERE username=$userFinal";
+	$sql = "SELECT username,password FROM user WHERE username=$userFinal";
 	$result = $connection->query($sql);
 	
 	//Mesaje pentru client la logare
 	if($result->num_rows == 0) {
-		header("Location: index.php?page=home&error=InvalidUserOrPass");
-		// echo "Username gresit!";
+		header("Location: index.php?page=home&error=InvalidUserorPassword");
 		exit;
 	}
 	
@@ -34,12 +31,14 @@ if (isset($_POST["access"]) ) {
 			
 			//Mesaje pentru client la logare
 			if("'".$usercheck."'" === $userFinal && "'".$passcheck."'" != $passwordFinal) {
-				die ("Parola gresita!");
+				header("Location: index.php?page=home&error=InvalidPassword");
+				exit;
 			}
-
+			
 			storeUserToSession($userFinal, $passwordFinal);
+			
+			}
 		}
 	}
-}
 
 ?>
--- PROIECT/pages/helpers/session.php
+++ PROIECT/pages/helpers/session.php
@@ -1,16 +1,17 @@
 <?php
 //Managementul sesiunii
 
-if(!isLoggedIn()) {
-    session_start();
-}
-
 function isLoggedIn() {
     return isset($_SESSION["user"]);
 }
 
-function storeUserToSession() {
+function storeUserToSession($userFinal,$passwordFinal) {
     //Managementul sesiunii
     $_SESSION["user"] = $userFinal;
     $_SESSION["password"] = $passwordFinal;
 }
+
+if(!isLoggedIn()) {
+    session_start();
+}
+?>
