92f420679558bf52e1a17b37aef995d1586e417c
gdincu <dici0137@scs.ubbcluj.ro>
2020-05-21 13:56:37 +0300

    Changed the DB_diagram

--- PROIECT/index.php
+++ PROIECT/index.php
@@ -1,7 +1,6 @@
 <?php
     require_once "db_setup/db_connect.php";
     require_once "pages/helpers/access.php";
-    include "pages/helpers/song.php";
     // https://stackoverflow.com/questions/2418473/difference-between-require-include-require-once-and-include-once
     
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
@@ -14,6 +13,7 @@
         $currentPage->renderHead();
         $currentPage->startBody();
         $currentPage->render();
+        include "pages/helpers/song.php";
         $currentPage->endBody();
     } else {
         require_once (__DIR__."/pages/ErrorPage.php");
