7d2b402d7aaac67ee3bf13a2d0702c13789f63d1
cristinabarnutiu <48153999+cristinabarnutiu@users.noreply.github.com>
2020-06-14 11:33:01 +0300

    Update playversity.sql
    added small script at the beginning to execute drop db if exists and create a new one

--- PROIECT/db_setup/playversity.sql
+++ PROIECT/db_setup/playversity.sql
@@ -7,6 +7,11 @@
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.1.28
 
+DROP DATABASE IF EXISTS playversity;
+
+CREATE DATABASE playversity;
+USE playversity;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
