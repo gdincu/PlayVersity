7a8f265908655f72ded657af22f02aecfc82091e
gdincu <dici0137@scs.ubbcluj.ro>
2020-06-23 11:03:53 +0300

    20200623 User delete functionality

--- PROIECT/pages/helpers/delete.php
+++ PROIECT/pages/helpers/delete.php
@@ -1,9 +1,21 @@
 <?php 
-// include (__DIR__ . '/../../db_setup/db_connect.php');
+/**
+ * If not logged in - re-directs to index.php
+ */
+if(!isset($_SESSION["user"]))
+header("Location: index.php");
 
-if (isset($_POST['yes'])) 
-echo "Yes";
+include (__DIR__ . '/../../db_setup/db_connect.php');
 
-if (isset($_POST['no'])) 
-echo "Yes";
+if (isset($_POST['btnSubmit'])) {
+
+$userFinal = "'" . $_SESSION["user"] . "'";
+$sql = "DELETE FROM user WHERE username = $userFinal";
+$connection->query($sql);
+
+//Re-directs to the sign-up page
+session_destroy();
+header("Location: index.php");
+exit;
+}
 ?>
