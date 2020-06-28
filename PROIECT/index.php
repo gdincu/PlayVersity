0147ce1e3d6310b47fff8585ee834ea762fe22cc
gdincu <dici0137@scs.ubbcluj.ro>
2020-06-28 10:53:01 +0300

    20200628 Added comments -3

--- PROIECT/index.php
+++ PROIECT/index.php
@@ -1,12 +1,12 @@
 <?php
     require_once "db_setup/db_connect.php";
     require_once "pages/helpers/access.php";
-    // https://stackoverflow.com/questions/2418473/difference-between-require-include-require-once-and-include-once
     
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 
     $requestedPage = ucwords($page) . 'Page';
     $pageLocation = 'pages/'. $page . 'Page.php';
+
     if (file_exists($pageLocation)) {
         require_once $pageLocation;
         $currentPage = new $requestedPage;
@@ -14,9 +14,11 @@
         $currentPage->startBody();
         $currentPage->render();
         $currentPage->endBody();
-    } else {
+        } 
+        else 
+        {
         require_once (__DIR__."/pages/ErrorPage.php");
         $currentPage = new ErrorPage();
         $currentPage->render();
-    }
+        }
 ?>
