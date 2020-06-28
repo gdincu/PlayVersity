40686f3ac197b6cbefd7fa45537cbf55431ac5a7
Oana <oan.max@gmail.com>
2020-06-28 12:11:28 +0300

    added image size

--- PROIECT/pages/helpers/uploadUserProfilePhoto.php
+++ PROIECT/pages/helpers/uploadUserProfilePhoto.php
@@ -8,33 +8,38 @@ if (!isLoggedIn()) {
 } else if(isset($_POST["saveChanges"])){ 
     if(!empty($_FILES["image"]["name"])) { 
         $status = 'error'; 
-        // Get file info 
-        $fileName = basename($_FILES["image"]["name"]); 
-        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
-         
-        // Allow certain file formats 
-        $allowTypes = array('jpg','png','jpeg','gif'); 
-        if(in_array($fileType, $allowTypes)){ 
-            $image = $_FILES['image']['tmp_name']; 
-            $imgContent = addslashes(file_get_contents($image)); 
-         
-            // Update image content into database
-            $user = getLoggedInUser(); 
-            $insert = $connection->query("UPDATE user SET image = '$imgContent' WHERE username='$user'"); 
-             
-            if($insert){ 
-                $status = 'success'; 
-                $statusMsg = "File uploaded successfully."; 
+        // Check file size
+        if ($_FILES["image"]["size"] > 500000) {
+            $statusMsg = "Sorry, your file is too large.";
+        } else {
+            // Get file info 
+            $fileName = basename($_FILES["image"]["name"]); 
+            $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+            // Allow certain file formats 
+            $allowTypes = array('jpg','png','jpeg','gif'); 
+            if(in_array($fileType, $allowTypes)){ 
+                $image = $_FILES['image']['tmp_name']; 
+                $imgContent = addslashes(file_get_contents($image)); 
+            
+                // Update image content into database
+                $user = getLoggedInUser(); 
+                $insert = $connection->query("UPDATE user SET image = '$imgContent' WHERE username='$user'"); 
+                
+                if($insert){ 
+                    $status = 'success'; 
+                    $statusMsg = "File uploaded successfully."; 
+                }else{ 
+                    $statusMsg = "File upload failed, please try again."; 
+                }  
             }else{ 
-                $statusMsg = "File upload failed, please try again."; 
-            }  
-        }else{ 
-            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
+                $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
+            }
         }
         
         
         if ($status != 'success') {
-            // Display status message 
+            // Display status message
+            $_SESSION["photo_size"] = 1; 
             echo $statusMsg; 
         }
     }
